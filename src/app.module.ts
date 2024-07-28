204a3a9fe4794c60f065deb385f9bcc651316532
evelin-code <f.gongoraandrade@gmail.com>
2024-07-28 16:18:41 -0500

    I set up master files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { UserNoSpecController } from './user--no-spec/user--no-spec.controller';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entity/user';
 
 @Module({
   imports: [
@@ -13,10 +10,11 @@ import { UserService } from './user/user.service';
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      entities: [],
+      entities: [User],
     }),
+    UserModule
   ],
-  controllers: [AppController, UserNoSpecController, UserController],
-  providers: [AppService, UserService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
