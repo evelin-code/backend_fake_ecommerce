0a0f58e936a4d03995340770008326346b3062de
evelin-code <f.gongoraandrade@gmail.com>
2024-07-30 23:48:06 -0500

    PORT in main for production

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import "reflect-metadata";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000
+  await app.listen(port);
 }
 bootstrap();
