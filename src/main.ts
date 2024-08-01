8a4eb18f605a2e0792c4b87e456f56eb09f7fa6e
evelin-code <f.gongoraandrade@gmail.com>
2024-07-31 20:05:38 -0500

    Configuration CORS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import "reflect-metadata";
+import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000
+
+  // Configuración de CORS
+  const allowedOrigins = process.env.NODE_ENV === 'production'
+    ? ['https://frontend-url.com'] // URL del frontend en producción
+    : ['http://localhost:5173']; // URL del frontend en desarrollo
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 }
+
 bootstrap();
